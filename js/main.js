b733a2dca43f4e2ff7c42126495c7e6d14c3fb55
Valentyna Boriak <valenkiri@gmail.com>
2024-04-30 22:09:50 +0300

    Add news section container and styles for it

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ $(() => {
       : $(".navbar-list__mobile-links").children().css("fill", "none");
   });
 
+  /*SLIDER IN HEADER*/
   $(".home-header__about-us-slider").slick({
     adaptiveHeight: true,
     appendDots: $(".home-header__dots-container"),
@@ -16,4 +17,29 @@ $(() => {
     arrows: false,
     customPaging: () => '<a class="dots__item" role="button"></a>',
   });
+
+  /*NEWS CARDS*/
+  // const cards = $(".news__card");
+  fetch("../assets/test-json/news.json")
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Request failed!");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      data.forEach((cardData) => {
+        const card = $("<div></div>");
+        const cardCover = $(
+          `<img src="${cardData.image_url}" alt=${cardData.img_description}/>`
+        );
+        cardCover.addClass("object-fit-contain");
+        const cardTitle = $(
+          `<h4 class="fs-m text-uppercase primary-txt-color mt-20 fw-bold">${cardData.title}</h4>`
+        );
+        card.append(cardCover, cardTitle);
+        card.addClass("card");
+        $(".news__slider").append(card);
+      });
+    });
 });
