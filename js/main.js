e2ee2905242f0a94236a36b87a3775062df39aa1
Valentyna Boriak <valenkiri@gmail.com>
2024-05-01 10:39:59 +0300

    Add all card info with JS

--- js/main.js
+++ js/main.js
@@ -19,7 +19,6 @@ $(() => {
   });
 
   /*NEWS CARDS*/
-  // const cards = $(".news__card");
   fetch("../assets/test-json/news.json")
     .then((res) => {
       if (!res.ok) {
@@ -30,14 +29,36 @@ $(() => {
     .then((data) => {
       data.forEach((cardData) => {
         const card = $("<div></div>");
+
+        //CARD TEXT
+        const cardInfo = $("<div></div>");
+        cardInfo.addClass("card-info");
         const cardCover = $(
           `<img src="${cardData.image_url}" alt=${cardData.img_description}/>`
         );
         cardCover.addClass("object-fit-contain");
-        const cardTitle = $(
-          `<h4 class="fs-m text-uppercase primary-txt-color mt-20 fw-bold">${cardData.title}</h4>`
+        const cardTitle = $(`<h4>${cardData.title}</h4>`);
+        cardTitle.addClass("fs-m text-uppercase primary-txt-color fw-bold");
+        const cardDescription = $(`<p>${cardData.snippet}</p>`);
+        cardDescription.addClass("primary-txt-color mt-20 secondary-font");
+
+        //AUTHOR INFO
+        const cardAuthorContainer = $("<div></div>");
+        cardAuthorContainer.addClass("card-author-container mt-40");
+        const cardAuthorImg = $(
+          `<img src="${cardData.author_img_url}" alt="${cardData.author}'s image"/>`
         );
-        card.append(cardCover, cardTitle);
+        cardAuthorImg.addClass("card-author-img");
+        const cardAuthorName = $(`<span>${cardData.author}</span>`);
+        cardAuthorName.addClass(
+          "card-author-name text-uppercase primary-txt-color"
+        );
+        const cardPostDate = $(`<span>${cardData.published_at}</span>`);
+        cardPostDate.addClass("card-post-date");
+
+        cardAuthorContainer.append(cardAuthorImg, cardAuthorName, cardPostDate);
+        cardInfo.append(cardTitle, cardDescription, cardAuthorContainer);
+        card.append(cardCover, cardInfo);
         card.addClass("card");
         $(".news__slider").append(card);
       });
