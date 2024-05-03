7e7875bcedc55aaa19f13d7dd3bb63c844f37e00
Valentyna Boriak <valenkiri@gmail.com>
2024-05-03 17:58:25 +0300

    Re-do the responsiveness of the gallery for the 550px breakpoint

--- js/main.js
+++ js/main.js
@@ -149,7 +149,7 @@ $(() => {
       data.forEach((image, index) => {
         if (index < 5) {
           const imgElem = $(`<img src=${image.url} alt=${image.name}/>`);
-          if (window.screen.width >= 1000) {
+          if (window.screen.width >= 550) {
             index === 0
               ? imgElem.addClass("img-1")
               : imgElem.addClass(`img-${index + 1}`);
